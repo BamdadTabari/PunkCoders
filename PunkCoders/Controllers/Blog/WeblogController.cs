bbf7ff73019977327567963a286f679e74376f6b
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2024-12-03 05:25:14 -0800

    wbc

---
+++ PunkCoders/Controllers/Blog/WeblogController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PunkCoders.Configs;
+
+namespace PunkCoders.Controllers.Blog;
+[Route("api/[controller]")]
+[ApiController]
+public class WeblogController : ControllerBase
+{
+    private readonly IMemoryCache _memoryCache;
+    private readonly CacheOptions _cacheOptions;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger _logger;
+    private const string CacheKey = "weblog";
+
+    public WeblogController(IMemoryCache memoryCache, IOptions<CacheOptions> cacheOptions, IUnitOfWork unitOfWork, ILogger logger)
+    {
+        _memoryCache = memoryCache;
+        _cacheOptions = cacheOptions.Value;
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("get-by-id")]
+    public async Task<IActionResult> Get([FromQuery] GetPostQuery getPostQuery)
+    {
+        string cacheKey = $"{CacheKey}_{getPostQuery.PostId}";
+
+        if (!_memoryCache.TryGetValue(cacheKey, out Post? result))
+        {
+            result = await _unitOfWork.PostRepo.GetByIdAsync(getPostQuery.PostId);
+
+            if (result == null) return NotFound("Post not found.");
+
+            _memoryCache.Set(cacheKey, result, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = _cacheOptions.AbsoluteExpiration,
+                SlidingExpiration = _cacheOptions.SlidingExpiration
+            });
+
+            CacheManager.AddKey(cacheKey);
+
+            result.Views += 1;
+            _unitOfWork.PostRepo.Update(result);
+        }
+        result.Views += 1;
+        _unitOfWork.PostRepo.Update(result);
+        _unitOfWork.Save();
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("get-by-filter")]
+    public IActionResult GetPaginated([FromQuery] GetPagedPostQuery getPagedPostQuery)
+    {
+        string cacheKey = $"{CacheKey}_Filter_Pagination";
+
+        if (!_memoryCache.TryGetValue(cacheKey, out PaginatedList<Post>? result))
+        {
+            result = _unitOfWork.PostRepo.GetPaginated(getPagedPostQuery);
+
+            _memoryCache.Set(cacheKey, result, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = _cacheOptions.AbsoluteExpiration,
+                SlidingExpiration = _cacheOptions.SlidingExpiration
+            });
+
+            CacheManager.AddKey(cacheKey);
+        }
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    [Route("clear-all-cache")]
+    public IActionResult ClearAllCache()
+    {
+        CacheManager.ClearKeysByPrefix(_memoryCache, CacheKey);
+        _logger.LogInformation("All cache for posts has been cleared At {Time}", DateTime.UtcNow);
+        return Ok("All cache for posts has been cleared.");
+    }
+
+}
