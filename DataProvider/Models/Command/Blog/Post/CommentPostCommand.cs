4456118a1e09d4f4f4a28af51c9ac0e1d793d1ec
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2024-12-06 00:27:41 -0800

    add commentpost command

---
+++ DataProvider/Models/Command/Blog/Post/CommentPostCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Models.Command.Blog.Post;
+public class CommentPostCommand
+{
+    public int PostId { get; set; }
+    public string CommentText { get; set; }
+}
